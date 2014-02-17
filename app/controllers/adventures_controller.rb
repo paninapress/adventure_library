3a24bafb6aa97a47897f6c3ebb4af43db385c589
paninapress <NPacifico25@gmail.com>
2014-02-16 16:56:37 -0800

    initial commit

---
+++ app/controllers/adventures_controller.rb
@@ -0,0 +1,19 @@
+class AdventuresController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
