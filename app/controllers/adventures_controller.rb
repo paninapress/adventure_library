49daef9c1ef90077d653672a9c440a35d25bb29a
paninapress <NPacifico25@gmail.com>
2014-02-16 17:37:39 -0800

    index/new/create works

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -1,11 +1,17 @@
 class AdventuresController < ApplicationController
   def index
+    @adventures = Adventure.all
   end
 
   def new
+    @adventure = Adventure.new
   end
 
   def create
+    adventure = params.require(:adventure).permit(:title, :author)
+    adv = Adventure.create(adventure)
+    adv.update_attributes(guid: SecureRandom.urlsafe_base64(10).to_s)
+    
   end
 
   def edit
